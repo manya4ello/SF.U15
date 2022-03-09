a789cdcf3cac8ea614081709a0d8d2f31c57f6e9
Oleg Freidkin <oleg.freidkin@gmail.com>
2022-03-09 03:37:06 +0300

    Добавьте файлы проекта.

---
+++ SF.U15/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] nums = { 1, 2, 3, 4, 5, 107};
+    
+Console.WriteLine(Average(nums));
+
+static double Average(int[] numbers)
+{
+     return (double) numbers.Average(x => x);
+
+}
