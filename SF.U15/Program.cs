41d8b2f60c3f8b22708103bcbc490cbadf581a0b
Oleg Freidkin <oleg.freidkin@gmail.com>
2022-03-10 20:00:17 +0300

    15.4.1

--- SF.U15/Program.cs
+++ SF.U15/Program.cs
@@ -1,12 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int[] nums = { 1, 2, 3, 4, 5, 107};
-    
-Console.WriteLine(Average(nums));
-
-static double Average(int[] numbers)
+﻿
+namespace linqlesson
 {
-     return (double) numbers.Average(x => x);
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 
+    public class Employee
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var departments = new List<Department>()
+{
+   new Department() {Id = 1, Name = "Программирование"},
+   new Department() {Id = 2, Name = "Продажи"}
+};
+
+            var employees = new List<Employee>()
+{
+   new Employee() { DepartmentId = 1, Name = "Инна", Id = 1},
+   new Employee() { DepartmentId = 1, Name = "Андрей", Id = 2},
+   new Employee() { DepartmentId = 2, Name = "Виктор ", Id = 3},
+   new Employee() { DepartmentId = 3, Name = "Альберт ", Id = 4},
+};
+
+            var employeeAndDep = from employee in employees
+                                 join dep in departments on employee.DepartmentId equals dep.Id //  соединяем коллекции по общему ключу
+                                 select new // выборка в новую сущность
+                                 {
+                                     EmployeeName = employee.Name,
+                                     DepartmentName = dep.Name
+                                 };
+
+            foreach (var item in employeeAndDep)
+                Console.WriteLine(item.EmployeeName + ", отдел: " + item.DepartmentName);
+        }
+    }
 }
